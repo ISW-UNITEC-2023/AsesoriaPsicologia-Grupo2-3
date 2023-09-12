ea4b09647c2b14c9b13f596e13db916bdd62e827
FerSosa64 <fernandososa03@unitec.edu>
2023-09-12 15:40:04 -0600

    Arreglo de anuncio
    Se agrego el boton de opciones para poder editar y eliminar un anuncio

--- portal-administrativo/frontend/asesoria-psicologica/src/pages/Anuncios/anuncios.jsx
+++ portal-administrativo/frontend/asesoria-psicologica/src/pages/Anuncios/anuncios.jsx
@@ -4,6 +4,8 @@ import "./anuncios.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate, Link } from "react-router-dom";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 
 function ComboBox() {
   return <select className="custom-combobox">{}</select>;
@@ -16,6 +18,12 @@ function SearchBar() {
 }
 
 function Anuncios() {
+  // Función para manejar el clic en "Editar anuncio"
+  const handleEditClick = () => {
+    // Coloca aquí la lógica para editar el anuncio
+    console.log("Editar anuncio");
+  };
+
   return (
     <div style={{ width: "90%" }}>
       <div className="container-header">
@@ -23,7 +31,10 @@ function Anuncios() {
         <a
           href="#"
           className="button-create"
-          onClick={"Colocar el link a la creacion de anuncios aqui"}
+          onClick={() => {
+            // Coloca aquí el enlace a la creación de anuncios
+            console.log("Redireccionar a la creación de anuncios");
+          }}
         >
           + Anuncios
         </a>
@@ -33,18 +44,32 @@ function Anuncios() {
           <Col md={4} lg={5}>
             <ComboBox />
           </Col>
-          <Col md={4} lg={6}>
+          <Col md={4} lg={7}>
             <SearchBar />
           </Col>
         </Row>
       </div>
       <ul>
         <li>
-          <div className="nombre-box">
+          <div
+            className="nombre-box"
+            style={{
+              display: "flex",
+            }}
+          >
             <FontAwesomeIcon icon={faUserCircle} className="icon-persona" />
             <div>
               <span className="titulo">{"Titulo"}</span>
-              <p className="descripcion">{"Descriprcion del anuncio"}</p>
+              <p className="descripcion">{"Descripción del anuncio"}</p>
+            </div>
+            <div
+              className="ml-auto"
+              style={{ alignSelf: "flex-end", marginLeft: "auto" }}
+            >
+              <DropdownButton id="dropdown-item-button">
+                <Dropdown.Item as="button">Editar</Dropdown.Item>
+                <Dropdown.Item as="button">Eliminar</Dropdown.Item>
+              </DropdownButton>
             </div>
           </div>
         </li>
