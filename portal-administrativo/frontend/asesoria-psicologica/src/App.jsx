bd0e5b76982156f8ce63267e22a3d025ed21405f
Alejandra Morales <61769099+amoralesc12@users.noreply.github.com>
2023-09-12 00:43:48 -0600

    Navbar- modificaciones
    Se agregaron los endpoints para las redirecciones de Inicio, Anuncios, Calendario, Cuentas, Historial.
    Tambien en el Navbar.jsx se agregaron style components.

--- portal-administrativo/frontend/asesoria-psicologica/src/App.jsx
+++ portal-administrativo/frontend/asesoria-psicologica/src/App.jsx
@@ -1,27 +1,38 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Reset from "./components/Reset/Navbar.jsx";
+import Reset from "./components/Reset/Reset";
 import ResetPass from "./pages/ResetPass";
-
-const Login = () => <div>Login</div>;
+import Modulos from "./pages/Modulos/Modulos";
+import Navbar from "./components/Navbar/Navbar"
+import PacientesForm from "./pages/Pacientes/Pacientes";
+import Expedientes from "./pages/Vistas/Vistas"
+import SectionsDashBoard from "./pages/DashBoard/DashBoard"
+import SectionsPage from "./pages/Sections/Sections";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/navbar">
-          {" "}
-          {/*iba el reset modifique para poder visual el navbar*/}
-          <Reset />
-        </Route>
-        <Route exact path="/forgot">
-          <Reset />
-        </Route>
-        <Route exact path="/reset-pass">
-          <ResetPass />
-        </Route>
-      </Switch>
+        <div style={{display:'flex', flexDirection:'row'}}>
+          <Navbar />
+          <Routes>
+                        {/*AQUI INICIAN LAS RUTAS DEL NAVBAR*/}
+            <Route path="/" element={<Modulos />} />
+            <Route path="/dashboard" element={<Modulos />} />
+            <Route path="/anuncios"/> 
+            <Route path="/calendar"/>
+            <Route path="/cuentas" element={<PacientesForm />} /> 
+            <Route path="/historial" /> 
+                          {/*AQUI FINALIZAN LAS RUTAS DEL NAVBAR*/}
+            <Route path="/reset" element={<Reset />} /> 
+            <Route path="/forgot" element={<Reset />} />
+            <Route path="/reset-pass" element={<ResetPass />} />
+            <Route path="/modulos" element={<Modulos />} /> 
+            <Route path="/expedientes" element={<Expedientes />} /> 
+            <Route path="/sections" element={<SectionsDashBoard />} /> 
+            <Route path="/sections/:courseId" element={<SectionsPage />} />{" "}
+          </Routes>
+        </div>
     </Router>
   );
 }
