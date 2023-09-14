06ae68b77fe615c38617e31d5b3e61145ad2098e
MIRIAN GEORGINA PINTO WELCHEZ <mirianpw1@unitec.edu>
2023-09-13 22:01:08 -0600

    Fusion rama CLases y Dessire Crear Anuncios

--- portal-administrativo/frontend/asesoria-psicologica/src/App.jsx
+++ portal-administrativo/frontend/asesoria-psicologica/src/App.jsx
@@ -15,12 +15,11 @@ import AnunciosPage from "./pages/AnunciosCrear/AnunciosCrear";
 function App() {
   return (
     <Router>
-        <div style={{display:'flex', flexDirection:'row'}}>  
+        <div style={{display:'flex', flexDirection:'row'}}>
           <Navbar />
           <Routes>
                       {/*AQUI INICIAN LAS RUTAS DEL NAVBAR*/}
             <Route path="/" element={<SectionsDashBoard />} />
-            <Route path="/inicio" element={<Modulos />} />
             <Route path="/dashboard" element={<SectionsDashBoard />} />
             <Route path="/anuncios" /> 
             <Route path="/clases" element={<SectionsDashBoard />} /> 
@@ -34,26 +33,13 @@ function App() {
             <Route path="/modulos" element={<Modulos />} /> 
             <Route path="/expedientes" element={<Expedientes />} /> 
             <Route path="/sections" element={<SectionsDashBoard />} /> 
-            <Route path="/sections/:courseId" element={<SectionsPage />} />{" "}
-          
-          </Routes>
-        </div>
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<AnunciosPage />} />
-          <Route path="/inicio" element={<Modulos />} />
-          <Route path="/reset" element={<Reset />} />{" "}
-          <Route path="/forgot" element={<Reset />} />
-          <Route path="/reset-pass" element={<ResetPass />} />
-          <Route path="/modulos" element={<Modulos />} />{" "}
-          <Route path="/expedientes" element={<Expedientes />} />{" "}
-          <Route path="/sections" element={<SectionsDashBoard />} />{" "}
+            <Route path="/pacientes" element={<PacientesForm />} />{" "}
+          {/*iba el reset modifique para poder visual el navbar*/}
           <Route path="/sections/:courseId" element={<SectionsPage />} />{" "}
            {/*iba el reset modifique para poder visual el navbar*/}
            <Route path="/anuncios" element={<AnunciosPage />} />{" "}
-        </Routes>
-      </div>
+          </Routes>
+        </div>
     </Router>
   );
 }
