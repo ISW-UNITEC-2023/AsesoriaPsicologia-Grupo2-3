d4d1dfc5528118f2a0a606920177ab3816d08c7f
Fernandolopez2171 <fernandolopez2171@unitec.edu>
2023-10-20 15:21:14 -0600

    Email ind/multiple
    Mecanismo de correos individuales y multiples agregados al backend

--- asesoria-isw-be/Controller/mail-controller.js
+++ asesoria-isw-be/Controller/mail-controller.js
@@ -1,17 +1,36 @@
-const emailsService = require('../Service/mail-services');
-
+const emailsService = require("../Service/mail-services");
+const nodemailer = require("nodemailer");
 //POST
 async function sendEmail(req, res) {
   const { to, subject, html, attachments } = req.body;
 
   try {
-    const info = await emailsService.sendEmail( to, subject, html, attachments);
+    const info = await emailsService.sendEmail(to, subject, html, attachments);
     res.json(info);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to send email' });
+    res.status(500).json({ error: "Failed to send email" });
+  }
+}
+
+async function sendEmailMultiple(req, res) {
+  const { name, email, subject, message, recipients } = req.body;
+
+  try {
+    const results = await emailsService.sendEmailMultiple(
+      name,
+      email,
+      subject,
+      message,
+      recipients
+    );
+    res.send("Correos enviados con éxito: " + results.join(", "));
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error al enviar los correos");
   }
 }
 
 module.exports = {
-  sendEmail
+  sendEmail,
+  sendEmailMultiple,
 };
--- asesoria-isw-be/Service/mail-services.js
+++ asesoria-isw-be/Service/mail-services.js
@@ -1,65 +1,103 @@
 var nodemailer = require("nodemailer");
 var Mailgen = require("mailgen");
 
-async function createMailTemplate(){
-    //Generador de Mailgen
-    let MailGenerator = new Mailgen({
-        theme: "default",
-        product: {
-        name: "UNITEC@ISW",
-        link: "https://unitec.edu/",
-        logo: "https://www.unitec.edu/res/img/logo-unitec-color.png",
-        logoHeight: "60px",
-        },
-    });
+async function createMailTemplate() {
+  //Generador de Mailgen
+  let MailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "UNITEC@ISW",
+      link: "https://unitec.edu/",
+      logo: "https://www.unitec.edu/res/img/logo-unitec-color.png",
+      logoHeight: "60px",
+    },
+  });
 
-    var email = {
-        body: {
-            name: "Mercedes Paz",
-            intro: "Ha recibido este correo por que se recibió su solicitud para reestablecer su contraseña",
-            action: {
-                instructions: "Haga click en el botón para reestablecer su contraseña",
-                button: {
-                    color: "#DC4D2F",
-                    text: "Reestablezca su contraseña",
-                    link: "https://mailgen.js/reset?s=b350163a1a010d9729feb74992c1a010",
-                },
-            },
-            outro: "Si no solicitó este cambio de contraseña, haga caso omiso a este correo.",
+  var email = {
+    body: {
+      name: "Mercedes Paz",
+      intro:
+        "Ha recibido este correo por que se recibió su solicitud para reestablecer su contraseña",
+      action: {
+        instructions: "Haga click en el botón para reestablecer su contraseña",
+        button: {
+          color: "#DC4D2F",
+          text: "Reestablezca su contraseña",
+          link: "https://mailgen.js/reset?s=b350163a1a010d9729feb74992c1a010",
         },
-    };
-    let mail = MailGenerator.generate(email);
-    return mail;
+      },
+      outro:
+        "Si no solicitó este cambio de contraseña, haga caso omiso a este correo.",
+    },
+  };
+  let mail = MailGenerator.generate(email);
+  return mail;
 }
 
 //POSTS
-async function sendEmail(body){
-    const { to, subject } = body;
-    const mail = await createMailTemplate();
-    //Transporter nodemailer
-    var transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: "asesoria.psicologica.unitec@gmail.com",
-            pass: "wjsnfbnwicipnkdi",
-        },
-    });
+async function sendEmail(body) {
+  const { to, subject } = body;
+  const mail = await createMailTemplate();
+  //Transporter nodemailer
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "asesoria.psicologica.unitec@gmail.com",
+      pass: "wjsnfbnwicipnkdi",
+    },
+  });
 
-    var mailOptions = {
-        from: "asesoria.psicologica.unitec@gmail.com",
-        to: to,
-        subject: subject,
-        html: mail,
-    };
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log("Email sent: " + info.response);
-        }
+  var mailOptions = {
+    from: "asesoria.psicologica.unitec@gmail.com",
+    to: to,
+    subject: subject,
+    html: mail,
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+}
+
+async function sendEmailMultiple(name, email, subject, message, recipients) {
+  const contentHTML = `
+      <h1>User Information</h1>
+      <ul>
+          <li>name: ${name}</li>
+          <li>Email: ${email}</li>
+      </ul>
+      <p>${message}</p>
+    `;
+
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const recipientEmails = recipients.split(",");
+
+  const sendPromises = recipientEmails.map(async (recipient) => {
+    const info = await transporter.sendMail({
+      from: "asesoria.psicologica.unitec@gmail.com",
+      to: recipient.trim(),
+      subject: subject,
+      html: contentHTML,
     });
+    console.log("Message sent to", recipient, info.messageId);
+    return `Correo enviado a ${recipient}`;
+  });
+
+  const results = await Promise.all(sendPromises);
+  return results;
 }
 
 module.exports = {
-    sendEmail
-}
+  sendEmail,
+  sendEmailMultiple,
+};
