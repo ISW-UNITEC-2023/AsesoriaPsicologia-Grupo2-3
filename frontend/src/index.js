27dea88280511172bc9c43b5c580a3ca35d4e3ce
david319 <larapineda2003@gmail.com>
2023-10-24 15:40:32 -0600

    LandingPage responsive, modificación en anuncios correcciones con tailwind

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 import App from './App';
 import {HelmetProvider} from "react-helmet-async";
+import './global.css';
+import 'normalize.css/normalize.css';
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(
