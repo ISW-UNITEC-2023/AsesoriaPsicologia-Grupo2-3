a412fef57a08e51b96a7fd931899ffc18f44c41a
sleepielle <pggisselle@gmail.com>
2023-11-05 12:29:32 -0600

    fix

--- frontend/src/Pages/DashBoard.jsx
+++ frontend/src/Pages/DashBoard.jsx
@@ -75,15 +75,12 @@ function DashBoard() {
             <br />
             <br />
             <div className="section-card-container flex flex-row flex-wrap gap-3 center">
-              {console.log(displayedModules)}
-              {displayedModules.coursesInfo.length != 0 && (
-                displayedModules.coursesInfo.map((module) => (
-                  <SectionCard
-                    props={module}
-                    handleReload={() => setState(true)}
-                  />
-                ))
-              )}
+              {displayedModules.map((module) => (
+                <SectionCard
+                  props={module}
+                  handleReload={() => setState(true)}
+                />
+              ))}
             </div>
           </div>
         </div>
