9f8745d5e6a0e984a26dbc17c4c82002536c1e83
dan29reyes <dan29reyes@unitec.edu>
2023-11-05 17:31:33 -0600

    filtros por asc, desc, rol y estado terminados

--- frontend/src/Pages/Accounts.jsx
+++ frontend/src/Pages/Accounts.jsx
@@ -2,6 +2,7 @@ import Navbar from "../Components/Navbar";
 import { useEffect, useState } from "react";
 import "../Styles/CSS/Accounts.css";
 import user_services from "../Utilities/user-services";
+import role_services from "../Utilities/roles-services";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faSearch,
@@ -14,11 +15,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function Accounts() {
   const [users, setUsers] = useState([]);
+  const [roles, setRoles] = useState([]);
   const [originalUsers, setOriginalUsers] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [matchingNames, setMatchingNames] = useState([]);
   const [displayResults, setDisplayResults] = useState(false);
   const [sorted, setSorted] = useState(false);
+  const [selectedRoles, setSelectedRoles] = useState([]);
+  const [selectedState, setSelectedState] = useState([]);
 
   //Mensajes de filtros
   const [isHovering, setIsHovering] = useState(false);
@@ -95,6 +99,92 @@ function Accounts() {
     );
   };
 
+  const CustomCbFilter = ({ type }) => {
+    return (
+      <div className="dropdown">
+        <button
+          className="custom-dropdown-toggle"
+          type="button"
+          id={`dropdownMenu$}`}
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          <FontAwesomeIcon icon={faFilter} className="filter-icon" />
+        </button>
+        <div className="dropdown-menu" aria-labelledby={`dropdownMenu${type}`}>
+          {type === "roles" ? (
+            <div className="form-check-filter">
+              {roles.map((role) => {
+                return (
+                  <label className="filter-check-label" htmlFor={role.id_role}>
+                    <input
+                      className="check-input-filter"
+                      type="checkbox"
+                      name={type}
+                      value={role.name_role}
+                      id={role.id_role}
+                      checked={selectedRoles.includes(role.name_role)}
+                      onChange={(e) => {
+                        filterSelectedItem(e);
+                      }}
+                    />
+                    {role.name_role}
+                  </label>
+                );
+              })}
+              <label className="filter-check-label">
+                <input
+                  className="check-input-filter"
+                  type="checkbox"
+                  name={type}
+                  value={"Sin rol"}
+                  id={0}
+                  checked={selectedRoles.includes("Sin rol")}
+                  onChange={(e) => {
+                    filterSelectedItem(e);
+                  }}
+                />
+                Sin rol
+              </label>
+            </div>
+          ) : (
+            <div className="form-check-filter">
+              <label className="filter-check-label" htmlFor="active">
+                <input
+                  className="check-input-filter"
+                  type="checkbox"
+                  name="state"
+                  value={1}
+                  id="active"
+                  checked={selectedState.includes(1)}
+                  onChange={(e) => {
+                    filterSelectedItem(e);
+                  }}
+                />
+                Activo
+              </label>
+              <label className="filter-check-label" htmlFor="inactive">
+                <input
+                  className="check-input-filter"
+                  type="checkbox"
+                  name={type}
+                  value={0}
+                  id="inactive"
+                  checked={selectedState.includes(0)}
+                  onChange={(e) => {
+                    filterSelectedItem(e);
+                  }}
+                />
+                Inactivo
+              </label>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  };
+
   const SearchDropdown = ({ matchingNames }) => {
     console.log(matchingNames.length);
     return (
@@ -152,6 +242,15 @@ function Accounts() {
     fetchData();
   }, []);
 
+  //Fetch de Roles
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedRoles = await role_services.getAllRoles();
+      setRoles(fetchedRoles);
+    };
+    fetchData();
+  }, []);
+
   //Filtrado de Nombre
   useEffect(() => {
     // Filter names that match the search term
@@ -188,8 +287,74 @@ function Accounts() {
   //Limpiar filtros
   function limpiarFiltros() {
     if (sorted) {
-      setUsers(originalUsers);
+      setSelectedRoles([]);
+      setSelectedState([]);
       setSorted(false);
+      setUsers(originalUsers);
+    }
+  }
+
+  function filterSelectedItem(e) {
+    let selectedItem = e.target.value;
+    let filteredUsers = [];
+    if (e.target.name === "roles") {
+      const newRoles = [...selectedRoles];
+      if (selectedRoles.includes(selectedItem)) {
+        const index = newRoles.indexOf(selectedItem);
+        newRoles.splice(index, 1);
+      } else {
+        if (selectedItem === "Sin rol") {
+          newRoles.push("Sin rol");
+        }
+        if (!newRoles.includes(selectedItem)) {
+          newRoles.push(selectedItem);
+        }
+      }
+      setSelectedRoles(newRoles);
+      originalUsers.filter((user) => {
+        newRoles.forEach((role) => {
+          if (user.roles.includes(role)) {
+            if (!filteredUsers.includes(user)) {
+              filteredUsers.push(user);
+            }
+          }
+          if (user.roles.length === 0 && role === "Sin rol") {
+            if (!filteredUsers.includes(user)) {
+              filteredUsers.push(user);
+            }
+          }
+        });
+      });
+      setSorted(true);
+      setUsers(filteredUsers);
+      if (newRoles.length === 0) {
+        limpiarFiltros();
+      }
+    } else if (e.target.name === "state") {
+      const states = [...selectedState];
+      selectedItem === "1" ? (selectedItem = 1) : (selectedItem = 0);
+      if (selectedState.includes(selectedItem)) {
+        const index = states.indexOf(selectedItem);
+        states.splice(index, 1);
+      } else {
+        states.push(selectedItem);
+      }
+      setSelectedState(states);
+      originalUsers.filter((user) => {
+        states.forEach((state) => {
+          if (user.active_user === state) {
+            if (!filteredUsers.includes(user)) {
+              filteredUsers.push(user);
+            }
+          }
+        });
+      });
+      setSorted(true);
+      setUsers(filteredUsers);
+      if (states.length === 0) {
+        console.log("limpiar filtros");
+        limpiarFiltros();
+      }
     }
   }
 
@@ -219,8 +384,14 @@ function Accounts() {
             onMouseOut={handleMouseOut}
           >
             <FontAwesomeIcon icon={faFilterCircleXmark} />
-            {isHovering && sorted && (<span className="limpiar-filtro-div">Limpiar filtros</span>)}
-            {isHovering && !sorted && (<span className="limpiar-filtro-div">No se han aplicado filtros</span>)}
+            {isHovering && sorted && (
+              <span className="limpiar-filtro-div">Limpiar filtros</span>
+            )}
+            {isHovering && !sorted && (
+              <span className="limpiar-filtro-div">
+                No se han aplicado filtros
+              </span>
+            )}
           </div>
           <button className="crear-cuenta-button">Crear cuenta</button>
         </div>
@@ -251,8 +422,18 @@ function Accounts() {
                   Número de teléfono
                 </div>
               </th>
-              <th>Rol</th>
-              <th>Estado</th>
+              <th>
+                <div className="th-div-account">
+                  <CustomCbFilter type="roles" />
+                  Rol
+                </div>
+              </th>
+              <th>
+                <div className="th-div-account">
+                  <CustomCbFilter type="state" />
+                  Estado
+                </div>
+              </th>
               <th>
                 <div className="th-div-account">
                   <CustomBtFilter type="creation_date" />
