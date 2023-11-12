312dc50e19c6e0c68e57377cc63a1a0e7ccf83d1
Liz-2711 <lizjrz11@unitec.edu>
2023-11-12 11:41:20 -0600

    Update Sesiones.jsx

--- frontend/src/Pages/Sesiones.jsx
+++ frontend/src/Pages/Sesiones.jsx
@@ -1,61 +1,67 @@
-import { Container, Row, Col } from "react-bootstrap";
-import "../Styles/CSS/Vistas.css";
-import tempImage from "../Styles/Images/tempprofile.png";
-import logoUnitec from "../Styles/Images/unitec-logo.png";
-import { Document, Page, Text, View, Image } from "@react-pdf/renderer";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import ListGroup from 'react-bootstrap/ListGroup';
 import "../Styles/CSS/Sesiones.css";
-import { Link } from "react-router-dom";
 
 function Sesiones() {
+  const [sessions, setSessions] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
+
+  const handleCreateSession = () => {
+    const newSession = sessions.length + 1;
+    setSessions([...sessions, newSession]);
+  };
+
+  const handleDeleteSession = (sessionNumber) => {
+    const updatedSessions = sessions.filter(session => session !== sessionNumber);
+    setSessions(updatedSessions);
+  };
+
   return (
     <div style={{ width: "90%" }}>
       <div className="container-header">
-        <h1 className="title-sesiones" style={{ width: "400%" }}>
+        <h1 className="title-sesiones" style={{ width: "100%", textAlign: "center", color: "#3498db" }}>
           Sesiones
         </h1>
+        <button
+          onClick={handleCreateSession}
+          style={{
+            padding: "10px",
+            backgroundColor: "#2ecc71",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+        >
+          Create Session
+        </button>
       </div>
       <ul>
-            <div className="nombre-box">
-              <span className="nombre" style={{ width: "400%" }}>
-                <Link to={"/Expedientes"}>Sesion 1</Link>
-              </span>
-            </div>
-            <div className="nombre-box">
-              <span className="nombre" style={{ width: "400%" }}>
-                <Link to={"/Expedientes"}>Sesion 2</Link>
-              </span>
-            </div>
-            <div className="nombre-box">
-              <span className="nombre" style={{ width: "400%" }}>
-                <Link to={"/Expedientes"}>Sesion 3</Link>
-              </span>
-            </div>
-            <div className="nombre-box">
-              <span className="nombre" style={{ width: "400%" }}>
-                <Link to={"/Expedientes"}>Sesion 4</Link>
-              </span>
-            </div>
-                        <div className="nombre-box">
-              <span className="nombre" style={{ width: "400%" }}>
-                <Link to={"/Expedientes"}>Sesion 5</Link>
-              </span>
-            </div>
-            <div className="nombre-box">
-              <span className="nombre" style={{ width: "400%" }}>
-                <Link to={"/Expedientes"}>Sesion 6</Link>
-              </span>
-            </div>
-            <div className="nombre-box">
-              <span className="nombre" style={{ width: "400%" }}>
-                <Link to={"/Expedientes"}>Sesion 7</Link>
-              </span>
-            </div>
-            <div className="nombre-box">
-              <span className="nombre" style={{ width: "400%" }}>
-                <Link to={"/Expedientes"}>Sesion 8</Link>
-              </span>
-            </div>
+        {sessions.map(sessionNumber => (
+          <div className="nombre-box" key={sessionNumber}>
+            <span className="nombre" style={{ width: "100%", display: "flex", justifyContent: "space-between" }}>
+              <Link
+                to={`/Expedientes/${sessionNumber}`}
+                style={{ color: "#2980b9", textDecoration: "none" }}
+              >
+                Sesion {sessionNumber}
+              </Link>
+              <button
+                onClick={() => handleDeleteSession(sessionNumber)}
+                style={{
+                  padding: "5px",
+                  backgroundColor: "#e74c3c",
+                  color: "white",
+                  border: "none",
+                  borderRadius: "3px",
+                  cursor: "pointer",
+                }}
+              >
+                Delete Session
+              </button>
+            </span>
+          </div>
+        ))}
       </ul>
     </div>
   );
