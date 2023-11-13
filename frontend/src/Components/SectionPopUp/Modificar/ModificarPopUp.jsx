345090407ecd4895fc931fdbcca42eee3e10bc4c
Carlos Nuñez <carlos_nc@unitec.edu>
2023-11-13 00:03:25 -0600

    Cambios de Oscar
    -

--- frontend/src/Components/SectionPopUp/Modificar/ModificarPopUp.jsx
+++ frontend/src/Components/SectionPopUp/Modificar/ModificarPopUp.jsx
@@ -4,8 +4,7 @@ import "../../../Styles/CSS/SectionPopUp/Modificar/ModificarPopUp.css";
 import propsTypes from "prop-types";
 
 import { Button } from "react-bootstrap";
-const ModificarPopUp = ({ isOpen, onClose, onConfirm, sectionId, Year, teacher, quarter, year }) => {
-  const [selectedOption, setSelectedOption] = useState("");
+const ModificarPopUp = ({ isOpen, onClose, onConfirm, sectionId, Year, teacher, quarter, year }) => {  const [selectedOption, setSelectedOption] = useState("");
   const [selectedTeacherOption, setSelectedTeacherOption] = useState([]);
   //---------------------------
   const [selectedTeacher, setSelectedTeacher] = useState("");
@@ -27,6 +26,7 @@ const ModificarPopUp = ({ isOpen, onClose, onConfirm, sectionId, Year, teacher, 
     async function fetchTeachers() {
       try {
         const response = await services.getTeachers();
+        console.log(response)
         setSelectedTeacherOption(response);
       } catch (error) {
         console.error("Error fetching teachers:", error);
@@ -74,113 +74,82 @@ const ModificarPopUp = ({ isOpen, onClose, onConfirm, sectionId, Year, teacher, 
           </h2>
 
           <div className="select-main">
-            <div className="select-container">
-              <label htmlFor="selectOption">Seleccione una opción:</label>
-              <select
-                id="selectOption"
-                value={selectedOption}
-                onChange={(e) => setSelectedOption(e.target.value)}
-                style={{ marginBottom: "20px" }}
-              >
-                <option value="">Seleccione una opción</option>
-                <option value="teacher_id">Teacher ID</option>
-                <option value="year">Year</option>
-                <option value="quarter">Quarter</option>
-                <option value="active">Estado</option>
-              </select>
-            </div>
-
-            <div className="select-container-two">
-              {selectedOption === "teacher_id" && (
-                <>
-                  <label htmlFor="selectTeacher">Seleccione un docente:</label>
-                  <select
-                    id="selectTeacher"
-                    value={selectedTeacher}
-                    onChange={(e) => setSelectedTeacher(e.target.value)}
-                  >
-                    <option value="">Seleccione un docente</option>
-                    {selectedTeacherOption.map((teacher, index) => (
-                      <option key={index} value={teacher.id_user}>
-                        {teacher.id_user} - {teacher.name_user}
-                      </option>
-                    ))}
-                  </select>
-                </>
-              )}
-              {selectedOption === "year" && (
-                <>
-                  <label htmlFor="selectYear">Seleccione un año:</label>
-                  <select
-                    id="selectYear"
-                    value={selectedYearOption}
-                    onChange={(e) => setSelectedYearOption(e.target.value)}
-                  >
-                    <option value="">Seleccione un año</option>
-                    {Array.from({ length: 11 }, (_, index) => (
-                      <option
-                        key={index}
-                        value={new Date().getFullYear() + index}
-                      >
-                        {new Date().getFullYear() + index}
-                      </option>
-                    ))}
-                  </select>
-                </>
-              )}
-              {selectedOption === "quarter" && (
-                <>
-                  <label htmlFor="selectQuarter">
-                    Seleccione un trimestre:
-                  </label>
-                  <select
-                    id="selectQuarter"
-                    value={selectedQuarterOption}
-                    onChange={(e) => setSelectedQuarterOption(e.target.value)}
-                  >
-                    <option value="">Seleccione un trimestre</option>
-                    {[1, 2, 3, 4].map((quarter) => (
-                      <option
-                        key={quarter}
-                        value={quarter}
-                        disabled={
-                          Year <= currentYear &&
-                          quarter < Math.floor((new Date().getMonth() + 3) / 3)
-                        }
-                      >
-                        Q{quarter}
-                      </option>
-                    ))}
-                  </select>
-                </>
-              )}
-              {selectedOption === "active" && (
-                <>
-                  <label htmlFor="selectActive">Seleccione un estado:</label>
-                  <select
-                    id="selectActive"
-                    value={selectedActiveOption}
-                    onChange={(e) => setSelectedActiveOption(e.target.value)}
-                  >
-                    <option value="">Seleccione un estado</option>
-                    <option value="1">Activo</option>
-                    <option value="0">Inactivo</option>
-                  </select>
-                </>
-              )}
-            </div>
+          <>
+              <div class="input-group mb-1">
+                <label htmlFor="selectTeacher"
+                      class="input-group-text"
+                      for="inputGroupSelect01">
+                  Seleccione un docente:
+                </label>
+                <select
+                  class="form-select"
+                  id="selectTeacher"
+                  value={selectedTeacher}
+                  onChange={(e) => setSelectedTeacher(e.target.value)}
+                >
+                  <option value="">{teacher}</option>
+                  {selectedTeacherOption.map((teacher, index) => (
+                    <option key={index} value={teacher.id}>
+                      {teacher.name_user}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </>
+            <>
+              <div class="input-group mb-1">
+                <label htmlFor="selectYear" class="input-group-text" for="inputGroupSelect01">Seleccione un año:</label>
+                <select
+                  class="form-select"
+                  id="selectYear"
+                  value={selectedYearOption}
+                  onChange={(e) => setSelectedYearOption(e.target.value)}
+                >
+                  <option value="">{year}</option>
+                  {Array.from({ length: 11 }, (_, index) => (
+                    <option
+                      key={index}
+                      value={new Date().getFullYear() + index}
+                    >
+                      {new Date().getFullYear() + index}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </>
+            <>
+              <div class="input-group mb-1">
+                <label htmlFor="selectQuarter" class="input-group-text" for="inputGroupSelect01">Seleccione un año:</label>
+                <select
+                  class="form-select"
+                  id="selectQuarter"
+                  value={selectedQuarterOption}
+                  onChange={(e) => setSelectedQuarterOption(e.target.value)}
+                >
+                  <option value="">Q{quarter}</option>
+                  {[1, 2, 3, 4].map((quarter) => (
+                    <option
+                      key={quarter}
+                      value={quarter}
+                      disabled={
+                        Year <= currentYear &&
+                        quarter < Math.floor((new Date().getMonth() + 3) / 3)
+                      }
+                    >
+                      Q{quarter}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </>
           </div>
 
           <div className="button-container-modificar">
-            <Button className="eliminar-section-popup" onClick={onClose}>
+            <Button className="eliminar-section-popup mb-3" onClick={onClose}>
               Cancelar
             </Button>
 
-            <Button
-              className="modificar-section-popup"
-              onClick={handleConfirm}
-              disabled={isButtonDisabled()}
-            >
+            <Button className="modificar-section-popup mb-3" onClick={handleConfirm}>
               Modificar
             </Button>
           </div>
