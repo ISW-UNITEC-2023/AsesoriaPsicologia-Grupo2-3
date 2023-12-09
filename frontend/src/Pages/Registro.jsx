9954534872de623480f931892557e386081bc0c6
Xaviertinoco06 <75238870+Xaviertinoco06@users.noreply.github.com>
2023-12-08 19:33:47 -0600

    Register FrontEnd

--- frontend/src/Pages/Registro.jsx
+++ frontend/src/Pages/Registro.jsx
@@ -3,6 +3,7 @@ import { Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom'; 
 import "../Styles/CSS/Registro.css";
 import NavigationBar from "../Components/NavigationBar";
+import RegisterDoctorImage from  "../Styles/Images/RegisterDoctor.png"
 
 function Wizard(props) {
     const [regData, setRegData] = useState({
@@ -13,10 +14,22 @@ function Wizard(props) {
         password: "",
         image: null,
     });
+    const [touchedFields, setTouchedFields] = useState({
+        name: false,
+        phoneNum: false,
+       
+    });
+
+    const validateData = (fieldName) => {
+        const value = regData[fieldName];
+        return value && value.length > 0;
+    };
     const [validations, setValidations] = useState({
         isPhoneNumValid: false,
         isEmailValid: false,
         isPasswordValid: false
+
+
     });
 
 
@@ -38,9 +51,8 @@ function Wizard(props) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Aquí puedes enviar los datos del formulario, incluida la imagen, a tu backend
-        // por ejemplo, usando fetch() o tu método preferido para enviar datos.
-        console.log(regData); // Esto muestra los datos en la consola para propósitos de demostración
+        
+        console.log(regData); 
     };
 
     return (
@@ -48,20 +60,22 @@ function Wizard(props) {
             <div className="navigation-bar">
                 <NavigationBar {...props} />
             </div>
+            <div className="content-wrapper">
+            <img src={RegisterDoctorImage} alt="Register Doctor" className="register-doctor-image" />
             <div className="form-container">
-
             <Form>
                                     <Form.Group controlId="formName" className="mb-3">
-                                        <Form.Label>Nombre Completo</Form.Label>
+                                        <Form.Label>Nombre De Clinica </Form.Label>
                                         <Form.Control
-                                            type="text"
-                                            name="name"
-                                            value={regData.name}
-                                            onChange={(e) => handleRegChange(e)}
-                                            placeholder="Ingrese su nombre completo"
-                                            isInvalid={!regData.name.length > 0}
-                                            required
-                                        />
+                                                type="text"
+                                                name="name"
+                                                value={regData.name}
+                                                onChange={(e) => handleRegChange(e)}
+                                                placeholder="Ingrese su nombre completo"
+                                                onBlur={() => setTouchedFields({ ...touchedFields, name: true })}
+                                                isInvalid={!regData.name.length > 0 && touchedFields.name}
+                                                required
+                                            />
                                         <Form.Control.Feedback type="invalid">
                                             El nombre no puede estar vacío.
                                         </Form.Control.Feedback>
@@ -75,58 +89,63 @@ function Wizard(props) {
                                             onChange={(e) => handleRegChange(e)}
                                             required
                                         />
-                                    </Form.Group>
-
-                                    
-                                        <Form.Group controlId="formImage" className="mb-3">
-                        <Form.Label>Subir imagen</Form.Label>
-                        <Form.Control
-                            type="file"
-                            name="image"
-                            onChange={handleImageChange}
-                            accept="image/*" // Acepta archivos de imagen
-                        />
                                         <Form.Control.Feedback type="invalid">
                                             El número de teléfono debe tener 8 dígitos.
                                         </Form.Control.Feedback>
                                     </Form.Group>
-                                    <Form.Group controlId="formEmail" className="mb-3">
-                                        <Form.Label>Correo Eléctronico</Form.Label>
+
+                                    <Form.Group controlId="formphoneNum" className="mb-3">
+                                        <Form.Label>Numero de Telefono de Clinica</Form.Label>
                                         <Form.Control
-                                            type="text"
-                                            name="email"
-                                            value={regData.email}
-                                            onChange={(e) => handleRegChange(e)}
-                                            onBlur={() =>
-                                                setValidations({
-                                                    ...validations,
-                                                    isEmailValid: valideData("email")
-                                                })
-                                            }
-                                            isInvalid={!validations.isEmailValid && !regData.email.length > 0}
-                                            required
-                                        />
+                                                type="number"
+                                                name="phoneNum"
+                                                value={regData.phoneNum}
+                                                onChange={(e) => handleRegChange(e)}
+                                                onBlur={() => setTouchedFields({ ...touchedFields, phoneNum: true })}
+                                                isInvalid={!regData.phoneNum.length > 0 && touchedFields.phoneNum}
+                                                required
+                                            />
                                         <Form.Control.Feedback type="invalid">
-                                            El correo electrónico debe ser válido.
+                                           Este campo solo acepta numeros
                                         </Form.Control.Feedback>
-                                    </Form.Group>
-                                    <Form.Group controlId="formPassword" className="mb-3">
-                                        <Form.Label>Contraseña</Form.Label>
-                                        <Form.Control
-                                            type="password"
-                                            name="password"
-                                            placeholder="Contraseña"
-                                            value={regData.password}
-                                            onChange={(e) => handleRegChange(e)}
-                                            onBlur={() =>
-                                                setValidations({
-                                                    ...validations,
-                                                    isPasswordValid: valideData("password")
-                                                })
-                                            }
-                                            isInvalid={!validations.isPasswordValid && !regData.password.length > 0}
-                                            required
-                                        />
+                                </Form.Group>
+                                            <Form.Group controlId="formEmail" className="mb-3">
+                                                <Form.Label>Correo Electrónico de Clínica</Form.Label>
+                                                <Form.Control
+                                                    type="text"
+                                                    name="email"
+                                                    value={regData.email}
+                                                    onChange={(e) => handleRegChange(e)}
+                                                    onBlur={() =>
+                                                        setValidations({
+                                                            ...validations,
+                                                            isEmailValid: validateData("email")
+                                                        })
+                                                    }
+                                                    isInvalid={!validations.isEmailValid && !regData.email.length > 0}
+                                                    required
+                                                />
+                                            <Form.Control.Feedback type="invalid">
+                                                El correo electrónico debe ser válido.
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                        <Form.Group controlId="formPassword" className="mb-3">
+                                            <Form.Label>Contraseña</Form.Label>
+                                            <Form.Control
+                                                type="password"
+                                                name="password"
+                                                placeholder="Contraseña"
+                                                value={regData.password}
+                                                onChange={(e) => handleRegChange(e)}
+                                                onBlur={() =>
+                                                    setValidations({
+                                                        ...validations,
+                                                        isPasswordValid: validateData("password")
+                                                    })
+                                                }
+                                                isInvalid={!validations.isPasswordValid && !regData.password.length > 0}
+                                                required
+                                            />
                                         <Form.Control.Feedback type="invalid">
                                             La contraseña debe tener al menos 8 caracteres y contener al menos una letra y un número.
                                         </Form.Control.Feedback>
@@ -137,7 +156,7 @@ function Wizard(props) {
                             type="file"
                             name="image"
                             onChange={handleImageChange}
-                            accept="image/*" // Acepta archivos de imagen
+                            accept="image/*" 
                         />
                     </Form.Group>
                 <Button className="button-reg" type="submit">
@@ -147,12 +166,13 @@ function Wizard(props) {
             <Link className="forgot-passwordR" to="/InicioSesion">
                 ¿Ya tienes una cuenta?
             </Link>
-
-            <div className="footer-general">
-                <p className="footerL">FUNDAEMPRESA UNITEC</p>
-                <p className="footerL">© 2023 - Todos los derechos reservados</p>
             </div>
         </div>
+
+            <div className="footer-general">
+                <p className="footerL">Clinica MEDICLOG</p>
+                <p className="footerL">© 2023 - Todos los derechos reservados</p>
+            </div>
         </div>
     );
 }
