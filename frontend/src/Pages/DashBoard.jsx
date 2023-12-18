d23bc462b9d61d8a61a04a4b64835135b642b409
Xaviertinoco06 <75238870+Xaviertinoco06@users.noreply.github.com>
2023-12-17 22:07:16 -0600

    detalles

--- frontend/src/Pages/DashBoard.jsx
+++ frontend/src/Pages/DashBoard.jsx
@@ -37,21 +37,36 @@ function DashBoard(props) {
         <div className="dashboard-box">
           <div className="dashboard-header flex flex-col md:flex-row justify-between">
             <h1 className="dashboard-titulo">{`Bienvenido ${nameUser}`}</h1>
-            </div>
-         
-            <div className="information-container">
-              <div className="information">
-                     
-              </div>
-              
-              </div>
-              <div className="dashed-line"></div>
-            </div>
+          </div>
+          <div className="information-container">
+            <div className="information">
           
+            </div>
+            <div>
+              <div className="quick-access-text">Acceso rápido al contenido</div>
+              <div className="dashed-line"></div>
+              <div className="button-container">
+                <button className="quick-access-button">Botón 1</button>
+                <button className="quick-access-button">Botón 2</button>
+                <div className="dropdown">
+                  <button className="quick-access-button dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                  </button>
+                  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a className="dropdown-item" href="#">Opción 1</a>
+                    <a className="dropdown-item" href="#">Opción 2</a>
+                    <a className="dropdown-item" href="#">Opción 3</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    
+      </div>
     </DashboardLayout>
   );
 }
+
+  
 
 export default DashBoard;
