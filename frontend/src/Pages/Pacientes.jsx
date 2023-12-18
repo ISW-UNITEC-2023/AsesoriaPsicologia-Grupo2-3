92e2236f2c68075fb06d17b4bc78b2e179c3c3bd
OscarS1247 <82965023+OscarS1247@users.noreply.github.com>
2023-12-17 18:09:57 -0600

    PopUp Crear Paciente

--- frontend/src/Pages/Pacientes.jsx
+++ frontend/src/Pages/Pacientes.jsx
@@ -1,24 +1,26 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "../Styles/CSS/Pacientes.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Services from "../Utilities/login-services";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faUserCircle} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import EditarUser from "../Components/PopUp_EditarUser";
 import CrearUser from "../Components/PopUp_CrearUser";
+import CrearPaciente from "../Components/CrearPaciente/PopUp_CrearPaciente";
 import NavigationB from "../Components/Navbar";
 import PacientesLayout from "../Layout/PacientesLayout";
 import axios from "axios";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 import useSWR from "swr";
 import user_services from "../Utilities/user-services";
-import {Option, Select, Spinner} from "@material-tailwind/react";
+import { Option, Select, Spinner } from "@material-tailwind/react";
 import Dropdown from "react-bootstrap/Dropdown";
 
 function PacientesForm(props) {
     const [nombres, setNombres] = useState([]);
     const [showCrearPopup, setShowCrearPopup] = useState(false);
     const [showEditarPopup, setShowEditarPopup] = useState(false);
+    const [showCrearPacientePopup, setShowCrearPacientePopup] = useState(false);
     const [selectedUser, setSelectedUser] = useState(null);
     const [showModal, setShowModal] = useState(false);
     const [montoConsulta, setMontoConsulta] = useState('');
@@ -61,6 +63,7 @@ function PacientesForm(props) {
     }
 
     const handleShow = () => setShowModal(true);
+
     const handleTerminarConsulta = () => {
         if (montoConsulta.trim() === '') {
             setMontoError(true);
@@ -88,6 +91,11 @@ function PacientesForm(props) {
             }
         }
     };
+    
+    const handleShowAgregarPacientePopUp = () => {
+        setShowCrearPacientePopup(true);
+    }
+
     const handleGuardarConsulta = () => {
         setMontoError(false);
         const doctorName = document.getElementById('doctorName').value;
@@ -153,6 +161,10 @@ function PacientesForm(props) {
         setSelectedUser(null);
     };
 
+    const closeCrearPacientePopup = () => {
+        setShowCrearPacientePopup(false);
+    };
+
     const handleClick = (id, nombre, id_clinic) => {
         localStorage.setItem('id_patient', id);
         localStorage.setItem('namePatient', nombre);
@@ -162,7 +174,7 @@ function PacientesForm(props) {
     if (usersLoading || rolesLoading) {
         return (
             <div className="flex justify-center items-center h-screen">
-                <Spinner/>
+                <Spinner />
             </div>
         )
     }
@@ -179,7 +191,7 @@ function PacientesForm(props) {
         const userRoles = fetchedRoles
             .filter(role => user.id_user === role.id_user)
             .map(role => [role.id_role, role.name_role]);
-        return {...user, roles: userRoles};
+        return { ...user, roles: userRoles };
     });
 
     const doctores = Array.isArray(usersWithRoles)
@@ -189,15 +201,14 @@ function PacientesForm(props) {
     return (
         <PacientesLayout pagina="Pacientes">
             <div className="navbar2">
-                <NavigationB/>
+                <NavigationB />
 
                 <div className="pacientes-container">
                     <div className="pacientes-header">
                         <h1 className="title-pacientes2">Pacientes</h1>
                         <div className="IniciarConsulta">
 
-                            <button className="consultation-btn" onClick={handleShow}>
-
+                            <button className="consultation-btn" onClick={handleShowAgregarPacientePopUp}>
                                 Iniciar consulta
                             </button>
                         </div>
@@ -206,41 +217,41 @@ function PacientesForm(props) {
                         {nombres.map((nombre) => (
                             <div key={nombre.id_account} className="card">
                                 <div className="card-body">
-                                    <FontAwesomeIcon icon={faUserCircle} className="icon-persona"/>
+                                    <FontAwesomeIcon icon={faUserCircle} className="icon-persona" />
                                     <h3 className="card-title">{nombre.nombre}</h3>
                                     <div className="conteiner-card-text">
                                         <h4 className="card-text">{formatDate(nombre.creationDate)}</h4>
                                     </div>
                                     <div className="dropdown">
                                         <button className="btn btn-secondary dropdown-toggle" type="button"
-                                                id={`dropdown-${nombre.id_account}`} data-toggle="dropdown"
-                                                aria-haspopup="true" aria-expanded="false">
+                                            id={`dropdown-${nombre.id_account}`} data-toggle="dropdown"
+                                            aria-haspopup="true" aria-expanded="false">
                                             Acciones
                                         </button>
 
                                         <div className="dropdown-menu"
-                                             aria-labelledby={`dropdown-${nombre.id_account}`}>
+                                            aria-labelledby={`dropdown-${nombre.id_account}`}>
                                             <Link to="/citas"
-                                                  onClick={() => handleClick(nombre.id_account, nombre.nombre,
-                                                      nombre.id_clinic)}
-                                                  className="dropdown-item">Agendar Cita</Link>
+                                                onClick={() => handleClick(nombre.id_account, nombre.nombre,
+                                                    nombre.id_clinic)}
+                                                className="dropdown-item">Agendar Cita</Link>
                                             <Link to="/Expedientes"
-                                                  onClick={() => handleClick(nombre.id_account, nombre.nombre,
-                                                      nombre.id_clinic)}
-                                                  className="dropdown-item">Ver Expediente</Link>
-                                                  <Link to="/Documentos" className="dropdown-item" state={{id_file:nombre.id_account, userData:props.userData}}>Ver Documentos</Link>
+                                                onClick={() => handleClick(nombre.id_account, nombre.nombre,
+                                                    nombre.id_clinic)}
+                                                className="dropdown-item">Ver Expediente</Link>
+                                            <Link to="/Documentos" className="dropdown-item" state={{ id_file: nombre.id_account, userData: props.userData }}>Ver Documentos</Link>
                                             {/* <Link to="/Documento"
                                                   onClick={() => handleClick(nombre.id_account, nombre.nombre,
                                                       nombre.id_clinic)}
                                                   className="dropdown-item">Ver Documentos</Link> */}
 
-{/* =======
+                                            {/* =======
                                         <div className="dropdown-menu" aria-labelledby={`dropdown-${nombre.id_account}`}>
                                             <Link to="/citas" className="dropdown-item">Manejar Cita</Link>
                                             <Link to="/Expedientes" className="dropdown-item">Ver Expediente</Link>
                                             <Link to="/Documentos" className="dropdown-item" state={{id_file:nombre.id_account, userData:props.userData}}>Ver Documentos</Link>
                                              */}
-{/* >>>>>>> Axel-KL-Documents */}
+                                            {/* >>>>>>> Axel-KL-Documents */}
                                         </div>
                                     </div>
                                 </div>
@@ -262,68 +273,14 @@ function PacientesForm(props) {
                             user={selectedUser}
                         />
                     )}
-                    <div className={`pop-iniciar-consulta ${showModal ? 'show' : ''}`}>
-                        <div className="pop-iniciar-consulta-content">
-                            <div className="pop-iniciar-consulta-header">
-                                <h1>Consulta Médica</h1>
-
-                                <buttons className="button-save" onClick={handleGuardarConsulta}>Guardar Consulta
-                                </buttons>
-                            </div>
-                            <div className="pop-iniciar-consulta-body">
-                                <div style={{display: 'flex', alignItems: 'center'}}>
-                                    <label htmlFor="doctorName">Nombre del Médico:</label>
-                                    <Select label="Nombre del médico" className="select-doctor"
-                                            placeholder="Seleccione un médico">
-                                        {doctores.map(doctor => (
-                                            <Option key={doctor.id_user} value={doctor.id_user} onClick={() => {
-                                                localStorage.setItem('id_doctor', doctor.id_user)
-                                            }}>
-                                                {doctor.name_user}
-                                            </Option>
-                                        ))}
-                                    </Select>
-                                </div>
-                                <form className="pop-iniciar-consulta-form">
-                                    <label htmlFor="consultaMotivo">Motivo de Consulta:</label>
-                                    <textarea id="consultaMotivo" rows={3}
-                                              placeholder="Ingrese motivo de consulta"
-                                              value={motivoConsulta}
-                                              onChange={(e) => setMotivoConsulta(e.target.value)}/>
-
-                                    <label htmlFor="observaciones">Observaciones:</label>
-                                    <textarea id="observaciones" rows={3}
-                                              placeholder="Ingrese observaciones"
-                                              value={observaciones}
-                                              onChange={(e) => setObservaciones(e.target.value)}/>
-                                    <label htmlFor="montoConsulta">Monto de Consulta:</label>
-                                    <div style={{display: 'flex', alignItems: 'center'}}>
-                                        <span style={{marginRight: '0.3rem'}}>Lps.</span>
-                                        <input type="text" id="montoConsulta" placeholder="Ingrese monto de consulta"
-                                               value={montoConsulta}
-                                               onChange={(e) => setMontoConsulta(e.target.value)}/>
-                                    </div>
-                                    {montoError && (
-                                        <p style={{color: "red", fontSize: "0.8rem", marginTop: "0.5rem"}}>El campo de
-                                            Monto de Consulta no puede estar vacío</p>
-                                    )}
-                                    <label htmlFor="ordenesMedicas">Órdenes Médicas:</label>
-                                    <textarea id="ordenesMedicas" rows={3}
-                                              placeholder="Ingrese órdenes médicas"
-                                              value={ordenesMedicas}
-                                              onChange={(e) => setOrdenesMedicas(e.target.value)}/>
-                                </form>
-                            </div>
-                            <div className="pop-iniciar-consulta-footer">
-                                <buttons className="close-button-sesiones" type="button" class="btn btn-outline-danger"
-                                         onClick={handleClose}>Cerrar
-                                </buttons>
-                                <buttons className="button-terminar" onClick={handleTerminarConsulta}>Terminar
-                                    Consulta
-                                </buttons>
-                            </div>
-                        </div>
-                    </div>
+                    {showCrearPacientePopup && (
+                        <CrearPaciente
+                            onClose={closeCrearPacientePopup}
+                            onSummit={addPacienteAndUpdateList}
+                            isOpen={showCrearPacientePopup}
+                        />
+                    )}
+                    
                 </div>
             </div>
         </PacientesLayout>
