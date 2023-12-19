398981830153177b5a545787f7b7c12fd166b743
Carlos Nuñez <carlos_nc@unitec.edu>
2023-12-19 16:38:51 -0600

    Privilegios de todos los roles enviados

--- asesoria-isw-be/Controller/users-controller.js
+++ asesoria-isw-be/Controller/users-controller.js
@@ -548,11 +548,14 @@ async function getVerify(req, res) {
       })
     );
 
-    console.log("Privilegios obtenidos", privileges);
+    const privilegios = [];
+    privileges.map((privilege_list) => {
+      privilege_list.map((privilegio) => {
+        privilegios.push(privilegio);
+      });
+    });
 
-    const privilegios = privileges[0]
-      ? privileges[0].map((privilege) => privilege.id_privilege)
-      : [];
+    console.log("Privilegios obtenidos", privilegios);
 
     const view = {
       roles: roleNames,
